e491f700287f4f23a125f3a98a34d12ad1957497
Clau Avila <clauavila.sch@gmail.com>
2019-02-06 16:51:24 -0300

    change error in def __init__

--- dino.py
+++ dino.py
@@ -2,9 +2,9 @@ class Dino:
     
     # cola, dientes, color, size
     def __init__(self,un_nombre='',un_color='verde'):   ## init es una funcion predefinida de python, se utiliza siempre al iniciar
-        nombre = un_nombre
-        color = un_color
-        print('Hola, soy un dinosaurio, me llamo', un_nombre,'y soy de color', un_color)
+        self.nombre = un_nombre
+        self.color = un_color
+        print('Hola, soy un dinosaurio, me llamo', self.nombre,'y soy de color', self.color)
     
 pepito = Dino("Pepe",'azul')
 pepita = Dino('Pepa','rojo')

89bfe48e5e39ad47c53d6bbd0e75c9778855eb34
Clau Avila <clauavila.sch@gmail.com>
2019-02-07 11:38:06 -0300

    Add 3 methods rugir, cambiar_color, que_color

--- dino.py
+++ dino.py
@@ -6,6 +6,21 @@ class Dino:
         self.color = un_color
         print('Hola, soy un dinosaurio, me llamo', self.nombre,'y soy de color', self.color)
 
+    def rugir (self, rugido):
+        print(self)
+        print (rugido)
+        print ('RAAAWR!!!', rugido)
+    
+    def cambiar_color(self, un_color):
+        self.color= un_color
+        ##print ('Se cambio el color de',self.nombre,'a', self.color)
+    
+    def que_color(self):
+        print('El color del dino es',self.color)
+
+pepito = Dino("Pepe",'azul') ##siempre hay que instanciar el objeto antes de ejecutar una funcion
+pepito.cambiar_color('amarillo') ##init es la funcion para instanciar y crear el objeto
+
 ## aca instaciamos varios objetos tipo Dino
 pepito = Dino("Pepe",'azul')
 pepita = Dino('Pepa','dorado')
